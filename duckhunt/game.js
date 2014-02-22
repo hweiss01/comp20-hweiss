e0efbc1aba7ecb243b0b5ca21d010e25c960a3e0
Hayley <hayley.weiss@tufts.edu>
2014-02-22 12:05:52 -0500

    added a canvas

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,18 @@
-// Your work goes here...
+
+
+
+    function draw() {
+        canvas = document.getElementById('game');
+        // Check if canvas is supported on browser
+        if (canvas.getContext) {
+        	console.log("We got the canvas!")
+            ctx = canvas.getContext('2d');
+             ctx.fillStyle = "rgb(0, 255, 0)";
+	           ctx.fillRect (50, 50, 55, 50);
+            
+        }
+        else {
+            alert('Sorry, canvas is not supported on your browser!');
+        }
+    }
+
