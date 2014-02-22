c22f2a1585c9f93f1d798007039bd33c32ad7b7c
Hayley <hayley.weiss@tufts.edu>
2014-02-22 13:13:49 -0500

    extended dirt

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -7,12 +7,14 @@
         if (canvas.getContext) {
 
             ctx = canvas.getContext('2d');
-            ctx.fillStyle = "#87CEEB"
-            ctx.fillRect(0, 0, 800, 100)
+            ctx.fillStyle = "#87CEEB";
+            ctx.fillRect(0, 0, 800, 600);
+            ctx.fillStyle = "C96A1B";
+            ctx.fillRect(0, 500, 800, 100);
             sprite = new Image();
             sprite.src = "assets/duckhunt.png";
             sprite.addEventListener("load", function() {
-            	  ctx.drawImage(sprite, 0, 713, 900, 190, 0, 450, 800, 190);
+            	  ctx.drawImage(sprite, 0, 713, 900, 190, 0, 400, 800, 190);
 
             	}, false);
           
