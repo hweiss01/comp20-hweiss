c83a976fb461d0c7b2717f5d310d2210ad0cf183
Hayley <hayley.weiss@tufts.edu>
2014-02-24 20:22:21 -0500

    final commit

---
+++ duckhunt/README.txt
@@ -0,0 +1,22 @@
+Hayley Weiss Assignment 2 part 1
+
+1. I used the sprite sheet and the drawImage() function to draw
+the required images onto the webpage.  Since an image needs to be loaded before
+it can be used in drawImage, I used 
+
+sprite.onload = function() { .. }
+
+and put my drawImage lines in here, as recommended. 
+
+Also, when I opened my file in Google Chrome it looked exactly how I wanted it
+to. But when I opened it in Firefox, there was a strip of blue sky at the 
+bottom of the canvas, making it look like the dirt didn't fully cover the sky,
+even though this didn't appear in Chrome. And when I opened it in Safari, it 
+only showed the rectangles I drew for the sky and the ground, and my first
+drawImage(), which is of the tree. None of the other drawImage() commands show
+up. I posted on Piazza and changed my code to what I wrote above as advised. 
+But the error still persists. I'm hoping you can tell me what I did wrong!
+
+2.  I didn't work with anyone
+
+3. It took about 3 hours.
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -10,10 +10,12 @@
             ctx.fillStyle = "#87CEEB";
             ctx.fillRect(0, 0, 800, 600);
             ctx.fillStyle = "C96A1B";
-            ctx.fillRect(0, 500, 800, 100);
-            sprite = new Image();
-            sprite.src = "assets/duckhunt.png";
-            sprite.addEventListener("load", function() {
+            ctx.fillRect(0, 500, 800, 200);
+            var sprite = document.createElement("img");
+
+            sprite.src = 'assets/duckhunt.png';
+            sprite.onload = function() {
+
             	  ctx.drawImage(sprite, 0, 273, 100, 116, 20, 35, 175, 450);
             	  ctx.drawImage(sprite, 0, 713, 900, 190, 0, 400, 800, 190);
             	  ctx.drawImage(sprite, 0, 0, 58, 50, 300, 470, 100, 100);
@@ -22,9 +24,9 @@
             	  ctx.drawImage(sprite, 335, 115, 39, 40, 325, 150, 70, 80);
             	  ctx.drawImage(sprite, 335, 190, 39, 40, 650, 350, 70, 80);
             	  ctx.drawImage(sprite, 205, 150, 39, 40, 350, 370, 70, 80);
-            	 
-            	}, false);
-          
+
+            	 }
+    
             
         }
         else {
